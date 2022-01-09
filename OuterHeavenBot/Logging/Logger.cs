2c4db5484f6bb98217613ba9acce1c5bc5ad2ad7
ryan bluestein <ryanbluestein7@gmail.com>
2022-01-09 12:28:55 -0500

    Add kill command as a temp fix for crashing. Added folder and classes for logging.

---
+++ OuterHeavenBot/Logging/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OuterHeavenBot.Logging
+{
+    class Logger<T>
+    {
+        string logPath;
+        public Logger(string logPath)
+        {
+            this.logPath = logPath;
+        }
+     
+        public void Log(object message)
+        {
+
+        }
+    }
+}
